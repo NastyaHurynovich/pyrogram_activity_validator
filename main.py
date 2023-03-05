aeebfe0596cc982e40c45a0876f90b314fe590bb
Nastya Hurynovich <nastia.gurinovich96@gmail.com>
2023-03-05 03:01:17 +0300

    Fixed client pool issues and translate logs to Russian

--- main.py
+++ main.py
@@ -2,28 +2,44 @@ import os
 import time
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
-
+from io import StringIO
+import sys
+import tgcrypto
 import pyrogram
-from telethon.errors import FloodWaitError
+from pyrogram.errors import FloodWait
 
-CREDENTIALS = []
 OUTPUT_FILE = "valid.txt"
 CLIENTS_POOL = []
 
+api_id = "18028667"
+api_hash = "9020cc8f73467f944b859c963519e5c5"
+
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio  # free up some memory
+        sys.stdout = self._stdout
+
 
 def setting_account():
-    # При старте на каждый из аккаунтов в консоли будет запрос на номер телефона, на который придёт код (его нужно
-    # тоже ввести). Для создания нескольких клиентов Вам нужно будет ввести уникальные номера телефонов
-    account = []
-    api_id = int(input("Введите данные аккаунта:\napi_id: "))
-    account.append(api_id)
-    api_hash = str(input("api_hash: "))
-    account.append(api_hash)
-    CREDENTIALS.append(account)
-    client = pyrogram.Client(f"anon.session_{api_id}", api_hash=api_hash, api_id=api_id)
-    CLIENTS_POOL.append(client)
-    client.start()
-    # client.disconnect()
+    try:
+        phone_number = input("Введите номер телефона: ")
+        print("Введите код доступа: ")
+        with Capturing() as output:
+            client = pyrogram.Client(f"anon.session_{datetime.now(tz=timezone.utc).timestamp()}", api_hash=api_hash,
+                                     api_id=api_id, phone_number=phone_number)
+            client.start()
+            CLIENTS_POOL.append(client)
+    except FloodWait:
+        print("Этот аккаунт забанен из-за флуда")
+    except Exception as e:
+        print("Ошибка при создании аккаунта: ", e)
     answer = input("Хотите ввести ещё один аккаунт? Y/n ")
     if answer.lower() == "y":
         setting_account()
@@ -49,16 +65,17 @@ def delete_duplicates(links):
 
 def check_criteria(pool_client, link, criteria, period):
     messages = list(
-        pool_client.get_chat_history(chat_id=link, limit=criteria, offset_date=datetime.now(tz=timezone.utc)))
+        pool_client.get_chat_history(chat_id=link, limit=(1 if criteria == 0 else criteria),
+                                     offset_date=datetime.now(tz=timezone.utc)))
     last_message = messages[-1]
-    return last_message.date.timestamp() >= (datetime.now(tz=timezone.utc) - timedelta(hours=period)).timestamp()
+    return (last_message.date.timestamp() >= (datetime.now(tz=timezone.utc) - timedelta(hours=period)).timestamp()) or \
+           (criteria == 0 and period == 0)
 
 
 def check_active_chat(links, pool_number, criteria, period):
     flood_errors_links = []
     links_handled_per_client = 0
     is_flood = False
-    print(f"Берем из пула клиент с номером {pool_number + 1}")
     curr_client = CLIENTS_POOL[pool_number]
     for link in links:
         # Меняем аккаунт каждые 20 каналов из списка или когда встречаем бан за Flood, а также если больше одного
@@ -70,21 +87,19 @@ def check_active_chat(links, pool_number, criteria, period):
                 pool_number = 0
             else:
                 pool_number += 1
-            print(f"Берем из пула клиент с номером {pool_number + 1}")
             curr_client = CLIENTS_POOL[pool_number]
         try:
             is_valid = check_criteria(curr_client, link, criteria, period)
-            print(link, ": ", is_valid)
+            print("@" + link, ": ", "валидная ссылка" if is_valid else "невалидная ссылка")
             if is_valid:
                 write_file(OUTPUT_FILE, link)
             links_handled_per_client += 1
-        except FloodWaitError as flood_error:
+        except FloodWait:
             flood_errors_links.append(link)
             is_flood = True
-            print("Flood error -> change client. Details: ", flood_error)
-        except Exception as e:
+        except Exception:
             links_handled_per_client += 1
-            print(f"Channel {link} is invalid or not exists. Details: ", e)
+            print("@" + link, ": невалидная ссылка")
         # Симулируем поведение человека, ждем 10 секунд перед обработкой следующей ссылки
         time.sleep(10)
 
@@ -107,6 +122,9 @@ def main():
     except Exception:
         pass
     setting_account()
+    if len(CLIENTS_POOL) == 0:
+        print("Аккаунт для работы не создан")
+        sys.exit()
     count_criteria = request_int("Введите количество сообщений для проверки 'активности' канала? ")
     period = request_int("Введите количество часов, за которые просматриваются сообщения и проверятся 'активность' "
                          "каналов? ")
